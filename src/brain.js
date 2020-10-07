dcb7c994015f81a40cf449b0bfd8397f466649a9
Marko <tomicmark3@gmail.com>
2020-10-07 19:54:25 +0200

    new commit

--- src/brain.js
+++ src/brain.js
@@ -4,11 +4,123 @@ import Look from "./look";
 class Brain extends React.Component {
   constructor(props) {
     super(props);
+    this.handleReset = this.handleReset.bind(this);
+    this.handleBreakDecrement = this.handleBreakDecrement.bind(this);
+    this.handleBreakIncrement = this.handleBreakIncrement.bind(this);
+    this.handleSessionDecrement = this.handleSessionDecrement.bind(this);
+    this.handleSessionIncrement = this.handleSessionIncrement.bind(this);
+    this.clock = this.clock.bind(this);
+    this.handlePlay = this.handlePlay.bind(this);
+    this.handleStop = this.handleStop.bind(this);
+
+    this.state = {
+      timer: 1500,
+      break: 5,
+      session: 25,
+      mode: true,
+    };
   }
+  handleReset() {
+    this.setState({
+      timer: 1500,
+      break: 5,
+      session: 25,
+    });
+    clearInterval(this.time1);
+    let sound = document.getElementById("beep");
+    sound.pause();
+    sound.currentTime = 0;
+  }
+  handleBreakDecrement() {
+    var br = this.state.break;
+
+    if (br > 1) {
+      this.setState({
+        break: br - 1,
+      });
+    }
+  }
+  handleSessionDecrement() {
+    var br = this.state.session;
+    var time = this.state.timer;
+    if (br > 1) {
+      this.setState({
+        session: br - 1,
+        timer: time - 60,
+      });
+    }
+  }
+  handleBreakIncrement() {
+    var br = this.state.break;
+    if (br < 60) {
+      this.setState({
+        break: br + 1,
+      });
+    }
+  }
+  handleSessionIncrement() {
+    var br = this.state.session;
+    var time = this.state.timer;
+    if (br < 60) {
+      this.setState({
+        session: br + 1,
+        timer: time + 60,
+      });
+    }
+  }
+  clock() {
+    let minutes = Math.floor(this.state.timer / 60);
+
+    let seconds = this.state.timer - minutes * 60;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    return minutes + ":" + seconds;
+  }
+
+  handlePlay() {
+    this.time1 = setInterval(() => {
+      if (this.state.timer < 2) {
+        var sound = document.getElementById("beep");
+        clearInterval(this.time1);
+        sound.play();
+      }
+      this.setState({
+        timer: this.state.timer - 1,
+      });
+    }, 1000);
+
+    this.setState({
+      mode: false,
+    });
+  }
+  handleStop() {
+    clearInterval(this.time1);
+    this.setState({
+      mode: true,
+    });
+  }
+
   render() {
     return (
       <div>
-        <Look />
+        <Look
+          session={this.state.session}
+          break={this.state.break}
+          timer={this.state.timer}
+          reset={this.handleReset}
+          Bdecrement={this.handleBreakDecrement}
+          Sdecrement={this.handleSessionDecrement}
+          Sincrement={this.handleSessionIncrement}
+          Bincrement={this.handleBreakIncrement}
+          time={this.clock}
+          play={this.handlePlay}
+          mode={this.state.mode}
+          stop={this.handleStop}
+        />
+        <audio
+          src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
+          id="beep"
+        ></audio>
       </div>
     );
   }
--- src/look.js
+++ src/look.js
@@ -3,44 +3,57 @@ import { BsArrowUp, BsArrowDown, BsPlay, BsPause } from "react-icons/bs";
 import { VscRefresh } from "react-icons/vsc";
 import "./look.css";
 
-function Look(props) {
-  return (
-    <div className="container">
-      <div id="title">25 + 5 Clock</div>
-      <div className="break">
-        <div id="break-label">Break Length</div>
-        <div id="break-decrement">
-          <BsArrowDown />
+class Look extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="container">
+        <div id="title">25 + 5 Clock</div>
+        <div className="break">
+          <div id="break-label">Break Length</div>
+          <div id="break-decrement" onClick={this.props.Bdecrement}>
+            <BsArrowDown size={25} />
+          </div>
+          <div id="break-length">{this.props.break}</div>
+          <div id="break-increment" onClick={this.props.Bincrement}>
+            <BsArrowUp size={25} />
+          </div>
         </div>
-        <div id="break-length">5</div>
-        <div id="break-increment">
-          <BsArrowUp />
+        <div className="session">
+          <div id="session-label">Session Length</div>
+          <div id="session-decrement" onClick={this.props.Sdecrement}>
+            <BsArrowDown size={25} />
+          </div>
+          <div id="session-length">{this.props.session}</div>
+          <div id="session-increment" onClick={this.props.Sincrement}>
+            <BsArrowUp size={25} />
+          </div>
+        </div>
+        <div className="timer">
+          <div id="timer-label">Session</div>
+          <div id="time-left">{this.props.time()}</div>
+        </div>
+        <div className="buttons">
+          <div
+            id="start_stop"
+            onClick={
+              this.props.mode === true ? this.props.play : this.props.stop
+            }
+          >
+            {this.props.mode === true ? (
+              <BsPlay size={25} />
+            ) : (
+              <BsPause size={25} />
+            )}
+          </div>
+          <div id="reset" onClick={this.props.reset}>
+            <VscRefresh size={25} />
+          </div>
         </div>
       </div>
-      <div className="session">
-        <div id="session-label">Session Length</div>
-        <div id="session-decrement">
-          <BsArrowDown />
-        </div>
-        <div id="session-length">25</div>
-        <div id="session-increment">
-          <BsArrowUp />
-        </div>
-      </div>
-      <div id="timer">
-        <div id="timer-label">Session</div>
-        <div id="time-left">25:00</div>
-      </div>
-      <div className="buttons">
-        <div id="start_stop">
-          <BsPlay />
-          <BsPause />
-        </div>
-        <div id="reset">
-          <VscRefresh />
-        </div>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 export default Look;
